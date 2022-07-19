b2d1c90d487ff2dea8367021d30064d68c93dde3
Joel Chang <jcchangg3@gmail.com>
2022-07-19 20:10:00 +0800

    Updates

---
+++ precache-manifest.21a779660504718f94827e12f7eb55f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2b99f05bbe6685a0715",
+    "url": "/body-fat-guesser/static/css/main.6bf35e0b.chunk.css"
+  },
+  {
+    "revision": "d2b99f05bbe6685a0715",
+    "url": "/body-fat-guesser/static/js/main.d2b99f05.chunk.js"
+  },
+  {
+    "revision": "3138a5955ecd3b381d13",
+    "url": "/body-fat-guesser/static/js/1.3138a595.chunk.js"
+  },
+  {
+    "revision": "5479158ccc6ba77ada82",
+    "url": "/body-fat-guesser/static/js/runtime~main.5479158c.js"
+  },
+  {
+    "revision": "0de6be830132aa4ad61d0a7984239b56",
+    "url": "/body-fat-guesser/index.html"
+  }
+];
